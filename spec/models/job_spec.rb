fe1a77c99e8447402c8812362e37926eab24ef77
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-04-20 10:44:36 -0700

    pass tests for presence validations on job

---
+++ spec/models/job_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe Job do
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :company }
+  it { should validate_presence_of :post_date }
+  it { should validate_presence_of :rating }
+  it { should validate_presence_of :location }
+  it { should validate_presence_of :url }
+end
