b1ecffcdc2106be0399a59b8de1718186de537e2
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-04-20 13:32:08 -0700

    test relationships

--- spec/models/step_spec.rb
+++ spec/models/step_spec.rb
@@ -4,4 +4,5 @@ describe Step do
   it { should validate_presence_of :description}
   it { should validate_presence_of :due_date }
   it { should validate_presence_of :priority }
+  it { should belong_to :job }
 end
