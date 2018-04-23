0fe7c2d185f34255214c728e7cb50f44887d1ab6
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-04-22 20:06:09 -0700

    fix step validation

--- app/models/step.rb
+++ app/models/step.rb
@@ -1,11 +1,11 @@
 class Step < ActiveRecord::Base
   belongs_to :job
   validates :description, :due_date, :priority, presence: true
-  validates :notes, length: { in: 50..250 }
+  validates :notes, length: { in: 10..250 }
 
   before_save :default_status
 
   def default_status
     self.status = false
-  end 
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,11 +4,11 @@ Step.destroy_all
 def add_default_steps(job)
 
   default_steps = [
-    {description: "LinkedIn Research", notes: "Notes...", due_date: Faker::Date.forward(10), priority: 1},
-    {description: "Write Cover Letter", notes: "Notes...", due_date: Faker::Date.forward(14), priority: 3},
-    {description: "Tailor Résumé", notes: "Notes...", due_date: Faker::Date.forward(14), priority: 3},
-    {description: "Apply", notes: "Notes...", due_date: Faker::Date.forward(17), priority: 3},
-    {description: "Followup", notes: "Notes...", due_date: Faker::Date.forward(21), priority: 2}
+    {description: "LinkedIn Research", notes: "Notes go here (10-250 characters)...", due_date: Faker::Date.forward(10), priority: 1},
+    {description: "Write Cover Letter", notes: "Notes go here (10-250 characters)...", due_date: Faker::Date.forward(14), priority: 3},
+    {description: "Tailor Résumé", notes: "Notes go here (10-250 characters)...", due_date: Faker::Date.forward(14), priority: 3},
+    {description: "Apply", notes: "Notes go here (10-250 characters)...", due_date: Faker::Date.forward(17), priority: 3},
+    {description: "Followup", notes: "Notes go here (10-250 characters)...", due_date: Faker::Date.forward(21), priority: 2}
   ]
 
   default_steps.each do |step|
@@ -32,4 +32,4 @@ end
 
 
 
-p "Created #{Job.count} jobs with 5 default steps"
+p "Created #{Job.count} jobs with #{Job.first.steps.length} default steps each"
