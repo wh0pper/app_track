50a65c25885c9ab008d119acedfb399400f7d419
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-04-22 19:38:47 -0700

    add default_status callback to step

--- app/models/step.rb
+++ app/models/step.rb
@@ -2,4 +2,10 @@ class Step < ActiveRecord::Base
   belongs_to :job
   validates :description, :due_date, :priority, presence: true
   validates :notes, length: { in: 50..250 }
+
+  before_save :default_status
+
+  def default_status
+    self.status = false
+  end 
 end
