005c50feea456d3e6034170059c8a7035d4238aa
jared clemmensen <jaredclemmensen@gmail.com>
2018-04-29 16:55:58 -0700

    add sign-up test, troubleshoot sign-in test.

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,14 +1,21 @@
 require 'rails_helper'
 
-describe "the sign-in process" do
-  it "logs in user and redirects to jobs index page" do
+describe 'the sign-up process' do
+  it 'signs up new user and redirects to jobs index page with a success message' do
     visit root_path
-    click_on "Log in"
-    fill_in "Email", with: "user1@example.com"
-    fill_in "user_password", with: "password"
-    save_and_open_page
+    fill_in "Email", with: 'test@test.com'
+    fill_in "Password", with: 'password'
+    fill_in "Password confirmation", with: 'password'
+    click_button "Sign up"
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
+  end
+end
 
-    click_on "Log in"
-    expect(page).to have_content "Signed in successfully"
+describe 'the sign-in process' do
+  it 'logs in user and redirects to jobs index page with a success message' do
+    user = FactoryBot.create(:user)
+    sign_in user
+    save_and_open_page
+    expect(page).to have_content 'Jobs List'
   end
 end
