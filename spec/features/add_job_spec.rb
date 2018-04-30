8ebaaf1a673eb415541a28f8bb64217c971f186c
jared clemmensen <jaredclemmensen@gmail.com>
2018-04-29 19:28:33 -0700

    pass integration test for adding a new step

--- spec/features/add_job_spec.rb
+++ spec/features/add_job_spec.rb
@@ -28,12 +28,16 @@ describe "crud functionality" do
     fill_in "Rating", with: 5
     fill_in "Location", with: "Portland, OR"
     fill_in "Url", with: "website.com"
+    click_button "Create Job"
     click_link "Test Job"
-    click_button "Add a step"
+    click_link "Add a step"
+    save_and_open_page
     fill_in "Description", with: "Test step"
     fill_in "Notes", with: "Just testin this out..."
     fill_in "Due date", with: Date.today.strftime('%Y-%m-%d')
     fill_in "Priority", with: 3
+    click_button "Create Step"
+    expect(page).to have_content("Test step")
   end
 
 end
