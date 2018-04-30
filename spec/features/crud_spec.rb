41230cfe8c865efd7a26797888bb9c3831029a49
jared clemmensen <jaredclemmensen@gmail.com>
2018-04-29 19:49:39 -0700

    troubleshoot alert problem in job deletion test

--- spec/features/crud_spec.rb
+++ spec/features/crud_spec.rb
@@ -11,14 +11,30 @@ describe "crud functionality" do
     fill_in "Title", with: "Test Job"
     fill_in "Company", with: "Test Company"
     fill_in "Post date", with: Date.today.strftime('%Y-%m-%d')
-    # select_date("04/20/2018", :from => "Post date")
     fill_in "Rating", with: 5
     fill_in "Location", with: "Portland, OR"
     fill_in "Url", with: "website.com"
-    save_and_open_page
     click_button "Create Job"
     expect(page).to have_content("Test Job")
   end
+
+  # it "deletes a job" do
+  #   visit root_path
+  #   fill_in "Title", with: "Test Job"
+  #   fill_in "Company", with: "Test Company"
+  #   fill_in "Post date", with: Date.today.strftime('%Y-%m-%d')
+  #   fill_in "Rating", with: 5
+  #   fill_in "Location", with: "Portland, OR"
+  #   fill_in "Url", with: "website.com"
+  #   click_button "Create Job"
+  #   click_link "Test Job"
+  #   # page.accept_alert do
+  #     click_button "Remove this job"
+  #   # end
+  #   page.execute_script("$(#{})")
+  #   save_and_open_page
+  #   expect(page).to have_no_content("Test Job")
+  # end
 
   it "adds a new step to a job" do
     visit root_path
@@ -31,7 +47,6 @@ describe "crud functionality" do
     click_button "Create Job"
     click_link "Test Job"
     click_link "Add a step"
-    save_and_open_page
     fill_in "Description", with: "Test step"
     fill_in "Notes", with: "Just testin this out..."
     fill_in "Due date", with: Date.today.strftime('%Y-%m-%d')
