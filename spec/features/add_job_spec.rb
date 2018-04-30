cacb2f7a58050726ab54ab12fc7df119ec70ff8d
jared clemmensen <jaredclemmensen@gmail.com>
2018-04-29 18:32:42 -0700

    fix date fill_in for add_job test

--- spec/features/add_job_spec.rb
+++ spec/features/add_job_spec.rb
@@ -1,1 +1,24 @@
 require 'rails_helper'
+
+describe "crud functionality" do
+  before :each do
+    user = User.create(email: 'user1@example.com', password: 'password')
+    sign_in user
+  end
+
+  it "creates a new job" do
+    visit root_path
+    fill_in "Title", with: "Test Job"
+    fill_in "Company", with: "Test Company"
+    fill_in "Post date", with: Date.today.strftime('%Y-%m-%d')
+    # select_date("04/20/2018", :from => "Post date")
+    fill_in "Rating", with: 5
+    fill_in "Location", with: "Portland, OR"
+    fill_in "Url", with: "website.com"
+    save_and_open_page
+    click_button "Create Job"
+    expect(page).to have_content("Test Job")
+  end
+
+
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,6 +32,7 @@ RSpec.configure do |config|
 
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
+  config.include Devise::Test::IntegrationHelpers
   config.include Warden::Test::Helpers
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
